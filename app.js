4a6d072c8723e9105c1d1785328ec4617610156a
Guillermo Del Vecchio <guillermo.deve@gmail.com>
2022-02-21 20:09:16 -0300

    feat(mining): add mining transactions

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const SHA256 = require("crypto-js/sha256");
 
+class Transaction {
+  constructor(fromAdress, toAddress, amount) {
+    this.fromAdress = fromAdress;
+    this.toAddress = toAddress;
+    this.amount = amount;
+  }
+}
+
 class Block {
-  constructor(timestamp, data, previousHash) {
+  constructor(timestamp, transactions, previousHash) {
     this.timestamp = timestamp;
-    this.data = data;
+    this.transactions = transactions;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
     this.seed = 0;
@@ -11,7 +19,10 @@ class Block {
 
   calculateHash() {
     return SHA256(
-      this.timestamp + this.previousHash + JSON.stringify(this.data) + this.seed
+      this.timestamp +
+        this.previousHash +
+        JSON.stringify(this.transactions) +
+        this.seed
     ).toString();
   }
 
@@ -28,20 +39,54 @@ class BlockChain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
     this.dificult = 3;
+    this.pendingTransactions = [];
+    this.miningReward = 10;
   }
 
   createGenesisBlock() {
-    return new Block(new Date(), "Genesis Block", "0");
+    return new Block(new Date(), [], "0");
   }
 
   getLastBlock() {
     return this.chain[this.chain.length - 1];
   }
 
-  addBlock(newBlock) {
-    newBlock.previousHash = this.getLastBlock().hash;
-    newBlock.findBlock(this.dificult);
-    this.chain.push(newBlock);
+  // addBlock(newBlock) {
+  //   newBlock.previousHash = this.getLastBlock().hash;
+  //   newBlock.findBlock(this.dificult);
+  //   this.chain.push(newBlock);
+  // }
+
+  addTransaction(transaction) {
+    this.pendingTransactions.push(transaction);
+  }
+
+  minePendingTransactions(minerAddress) {
+    let block = new Block(new Date(), this.pendingTransactions);
+
+    block.previousHash = this.getLastBlock().hash;
+    block.findBlock(this.dificult);
+
+    console.log("New Block has been mined");
+    this.chain.push(block);
+    this.pendingTransactions = [
+      new Transaction(null, minerAddress, this.miningReward),
+    ];
+  }
+
+  getBalanceOfAddress(address) {
+    let balance = 0;
+    for (const block of this.chain) {
+      for (const trans of block.transactions) {
+        if (trans.fromAdress === address) {
+          balance -= trans.amount;
+        }
+        if (trans.toAddress === address) {
+          balance += trans.amount;
+        }
+      }
+    }
+    return balance;
   }
 
   validateChain() {
@@ -61,15 +106,30 @@ class BlockChain {
 }
 
 let ervitium = new BlockChain();
-console.log("Mining block");
-ervitium.addBlock(new Block(new Date(), { quantity: 10 }));
-console.log("Mining block");
-ervitium.addBlock(new Block(new Date(), { quantity: 2 }));
-console.log("Mining block");
-ervitium.addBlock(new Block(new Date(), { quantity: 1 }));
-console.log("Mining block");
-ervitium.addBlock(new Block(new Date(), { quantity: 1 }));
-console.log("Finished...");
+// console.log("Mining block");
+// ervitium.addBlock(new Block(new Date(), { quantity: 10 }));
+// console.log("Mining block");
+// ervitium.addBlock(new Block(new Date(), { quantity: 2 }));
+// console.log("Mining block");
+// ervitium.addBlock(new Block(new Date(), { quantity: 1 }));
+// console.log("Mining block");
+// ervitium.addBlock(new Block(new Date(), { quantity: 1 }));
+
+ervitium.addTransaction(new Transaction("000xErvity", "000xChicho", 100));
+ervitium.addTransaction(new Transaction("000xJuan", "000xChicho", 100));
+ervitium.addTransaction(new Transaction("000xMaxy", "000xChicho", 50));
+
+console.log("starting mining");
+ervitium.minePendingTransactions("000xMiner");
 
 console.log(JSON.stringify(ervitium, null, 4));
+
+console.log(
+  "Balance de 000xChicho:",
+  ervitium.getBalanceOfAddress("000xChicho")
+);
+console.log("Balance de 000xMiner:", ervitium.getBalanceOfAddress("000xMiner"));
+
+console.log("Finished...");
+
 console.log(ervitium.validateChain());
